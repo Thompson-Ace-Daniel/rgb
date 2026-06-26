bac8ca56678d6f890c15665a3d1d0fff2bb3b68a
Otaifoh Godsent <osegiegodsent59@gmail.com>
2026-06-26 07:08:25 +0100

    reset RGB frontend

--- rgb-frontend/app/_layout.tsx
+++ rgb-frontend/app/_layout.tsx
@@ -1,52 +1,7 @@
- import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-import "@/global.css";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import { ActivityIndicator, View } from "react-native";
-import { use, useState } from "react";
-import Index from "./index";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [initializing, setInitializing] = useState(true);
+  return <Slot/>
 
-  if(initializing){
-    return(
-      <Index></Index>
-    )
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <SafeAreaProvider>
-        <SafeAreaView className="flex-1">
-          <Stack>
-            <Stack.Screen
-              name="(account-panel)"
-              options={{ headerShown: false }}
-            />
-
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-            <Stack.Screen name="(screens)" options={{ headerShown: false }} />
-
-            <Stack.Screen
-              name="modal"
-              options={{ presentation: "modal", title: "Modal" }}
-            />
-          </Stack>
-        </SafeAreaView>
-      </SafeAreaProvider>
-      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} backgroundColor={colorScheme === "dark" ? "#000000" : "#ffffff"} translucent={false} />
-    </ThemeProvider>
-  );
 }
- 
--- rgb-frontend/app/index.tsx
+++ rgb-frontend/app/index.tsx
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-    return(
-        <Text>Hello Godsent</Text>
-    )
+  return (
+        <SafeAreaView>
+          <View>
+            <Text>RGB Frontend</Text>
+          </View>
+        </SafeAreaView>
+  );
 }
